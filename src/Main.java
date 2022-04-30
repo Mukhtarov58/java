6dba48291ad77f36a40dbeb1085e01329b1bf73a
Timur <timur.pnz@mail.ru>
2022-04-30 17:47:14 +0300

    Milk price code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int milk_price;
+        milk_price = 80;
+        System.out.println(milk_price);
+        int count;
+        count = 4;
+        System.out.println(count);
+        int total = milk_price * count + 120;
+        System.out.println(total);
+    }
+}
