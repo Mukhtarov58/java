5341a1fced1d1f65795f68dee06464363805e593
Timur <timur.pnz@mail.ru>
2022-05-01 18:59:32 +0300

    Bonuse price

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean bayticket = true;
+        long price = 1500;
+        long bonus = 20;
+        long score_bonuse = price / bonus;
+        System.out.println(score_bonuse);
+    }
+}
+
+// Расчёт бонуса при прокупке билета.
