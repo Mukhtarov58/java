25f02c0b3dd34186f60670256d6e60936b31a095
Timur <timur.pnz@mail.ru>
2022-05-03 22:51:34 +0300

    Project5 done

---
+++ src/mine.java
@@ -0,0 +1,14 @@
+public class mine {
+    public static void main(String[] args) {
+        long score_client = 100;
+        long supplement =  1100;
+        long bonus = 0;
+        if (supplement > 1000) {
+            bonus = supplement / 100;
+        }
+        long sum = score_client + supplement + bonus;
+        System.out.println(sum);
+
+
+    }
+}
